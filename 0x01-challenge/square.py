5a2c73ae0996d2493d8f074489ccfb7178560c2e
3zzazakl <vanzekovichgr@gmail.com>
2024-05-04 19:11:28 +0300

    checking square class name

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
+"""Square class"""
 
-class square():
-    
+
+class Square():
+    """Square class"""
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """Initialize a new square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
+        """Return the area of the square"""
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
+        """Return the perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Return the square dimensions"""
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    """Main"""
+
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
